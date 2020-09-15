a6b4c21d4a8bb893a76bd21512c32d9c978d7318
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-09-15 13:46:29 -0400

    adding constructors, event listener and prototype

---
+++ app.js
@@ -0,0 +1,45 @@
+//Book Constructor - creates Book object
+function Book(title, author, isbn){
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+//UI Constructor - set of prototype methods
+function UI(){
+
+  UI.prototype.addBookToList = function(book){
+    console.log(book)
+    const list = document.getElementById('book-list');
+    //create tr element
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a></td>
+    `;
+    list.appendChild(row);
+  };
+}
+
+//Event Listeners
+document.getElementById('book-form').addEventListener('submit', 
+function(e){
+  //form values
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
+  
+  //instatiate book
+  const book = new Book(title, author, isbn)
+
+  //instantiate UI 
+  const ui = new UI();
+
+  //Add book to list
+  ui.addBookToList(book);
+  
+
+  e.preventDefault();
+})
